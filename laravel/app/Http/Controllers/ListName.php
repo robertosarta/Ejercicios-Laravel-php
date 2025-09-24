fcb1bd24af2d5f3ae706073428d121b2fb73c6bc
Roberto <robertosarta.rm@gmail.com>
2025-09-24 19:42:28 +0200

    Añado proyecto de laravel para ejercicio 2

---
+++ laravel/app/Http/Controllers/ListName.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListName extends Controller
+{
+    function show() {
+        return view('list_name');
+    }
+}
---
+++ laravel/resources/views/list_name.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main')
+
+@section('content')
+    <h1>LISTADO</h1>
+@endsection
+
