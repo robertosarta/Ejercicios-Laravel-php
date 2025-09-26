b1277db93024fdccbaa2e918710812365681d8b2
Roberto <robertosarta.rm@gmail.com>
2025-09-27 00:09:37 +0200

    elimino archivos antiguos de PruebaMal

--- PruebaMal/resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout.main')
-
-@section('content')
-<h1>Sobre mí</h1>
-<p>Mi nombre es {{ $name }} y el último libro que leí fue "{{ $lastBook }}".</p>
-@endsection
--- PruebaMal/resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout.main')
-
-@section('content')
-    <h1>Benvenutto al ejercicio nº 3</h1>    
-@endsection
